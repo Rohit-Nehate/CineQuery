9014083e7063f77b4080efeefecca83112c33f17
Rohit Nehate <tallertube@gmail.com>
2026-01-24 22:23:30 +0530

    improved and added new elems

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -39,11 +39,23 @@ if(!watchlist)return null;
 <div className='pt-30! px-12!'>
 
 <button onClick={handleClearWatchlist} className="mt-4 px-6! py-2! cursor-pointer bg-red-600 text-white rounded-full hover:bg-red-800 transition-colors duration-100">CLEAR WATCHLIST</button>
+
+{watchlist && watchlist.length > 0 ? 
 <div className='flex gap-5 flex-wrap mt-8! mb-8!'>
 
-{watchlist || watchlist.length > 0 ? watchlist.map(movie=> <MovieCard key={movie.id} movie={movie} type={movie.type}/>):<h1>NO MOVIES IN WATCHLIST</h1>}
+{watchlist.map(movie=> <MovieCard key={movie.id} movie={movie} type={movie.type}/>)}
 
-</div>
+</div>:
+<div className='flex justify-center items-center'>
+  
+  
+<h1 className='text-white text-5xl'>
+  NO ITEMS IN WATCHLIST
+</h1>
+
+   </div>
+
+}
 
 </div>
 
